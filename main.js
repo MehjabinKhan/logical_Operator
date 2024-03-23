24ae767dfe5d113df5ddb6b65ead25ba295d2e0f
MehjabinKhan <161743992+MehjabinKhan@users.noreply.github.com>
2024-03-23 21:31:15 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+"use strict";
+//          &&
+let mubashirBudget = 500000;
+let watchPrice = 400000;
+console.log(mubashirBudget && watchPrice);
+let daysInAYear = 365;
+let daysInAWeek = 7;
+console.log(daysInAYear && daysInAWeek);
+let haveDegree = true;
+let haveJob = false;
+console.log(haveDegree && haveJob);
+//       ||
+let isRaining = true;
+let isCold = false;
+console.log(isRaining || isCold);
+let n = 5 || 10;
+console.log(n);
+//       !
+let x = 6;
+console.log(x != 10);
+let haveStudied = "yes";
+let gaveExam = "no";
+console.log(haveStudied != gaveExam);
